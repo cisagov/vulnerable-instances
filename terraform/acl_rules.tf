3b5a42d2bba5c3dea9d8dcb1a9edfa2ef185fe71
Dave Redmin <david.redmin@beta.dhs.gov>
2019-06-06 18:32:44 -0400

    Run terraform 0.12upgrade
    These changes are all from running the terraform 0.12upgrade script

--- terraform/acl_rules.tf
+++ terraform/acl_rules.tf
@@ -1,7 +1,7 @@
 # Allow ingress from anywhere via any protocol and port
 # Restrictions by source IP are handled by the security group rules
 resource "aws_network_acl_rule" "ingress_from_anywhere_via_any_port" {
-  network_acl_id = "${aws_network_acl.vuln_acl.id}"
+  network_acl_id = aws_network_acl.vuln_acl.id
   egress         = false
   protocol       = "-1"
   rule_number    = 100
@@ -14,7 +14,7 @@ resource "aws_network_acl_rule" "ingress_from_anywhere_via_any_port" {
 # Allow egress to anywhere via any protocol and port
 # Restrictions by source IP are handled by the security group rules
 resource "aws_network_acl_rule" "egress_to_anywhere_via_any_port" {
-  network_acl_id = "${aws_network_acl.vuln_acl.id}"
+  network_acl_id = aws_network_acl.vuln_acl.id
   egress         = true
   protocol       = "-1"
   rule_number    = 110
--- terraform/ubuntu1404_ec2.tf
+++ terraform/ubuntu1404_ec2.tf
@@ -17,17 +17,17 @@ data "aws_ami" "metasploitable3_ubuntu1404" {
     values = ["ebs"]
   }
 
-  owners      = ["${data.aws_caller_identity.current.account_id}"] # This is us
+  owners      = [data.aws_caller_identity.current.account_id] # This is us
   most_recent = true
 }
 
 # The vulnerable Ubuntu 14.04 EC2 instance
 resource "aws_instance" "vuln_ubuntu1404" {
-  ami               = "${data.aws_ami.metasploitable3_ubuntu1404.id}"
+  ami               = data.aws_ami.metasploitable3_ubuntu1404.id
   instance_type     = "t2.micro"
   availability_zone = "${var.aws_region}${var.aws_availability_zone}"
 
-  subnet_id                   = "${aws_subnet.vuln_subnet.id}"
+  subnet_id                   = aws_subnet.vuln_subnet.id
   associate_public_ip_address = true
 
   root_block_device {
@@ -37,11 +37,21 @@ resource "aws_instance" "vuln_ubuntu1404" {
   }
 
   vpc_security_group_ids = [
-    "${aws_security_group.vuln_ubuntu1404_sg.id}",
+    aws_security_group.vuln_ubuntu1404_sg.id,
   ]
 
-  user_data_base64 = "${data.template_cloudinit_config.ssh_cloud_init_tasks.rendered}"
+  user_data_base64 = data.template_cloudinit_config.ssh_cloud_init_tasks.rendered
 
-  tags        = "${merge(var.tags, map("Name", "Vulnerable Ubuntu 14.04"))}"
-  volume_tags = "${merge(var.tags, map("Name", "Vulnerable Ubuntu 14.04"))}"
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "Vulnerable Ubuntu 14.04"
+    },
+  )
+  volume_tags = merge(
+    var.tags,
+    {
+      "Name" = "Vulnerable Ubuntu 14.04"
+    },
+  )
 }
