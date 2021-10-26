f70410061b8c6b9249895571e05dfb7a142efb18
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-10-26 16:36:25 -0400

    Upgrade the root volume type to gp3
    gp3 is 20% cheaper, and the baseline configuration offers better
    performance than gp2 for volumes smaller than 2TB.  It also allows the
    volume size and IOPS to be configured separately, whereas the two are
    intertwined with gp2.
    
    Also remove the delete_on_termination line, since
    delete_on_termination = true is the default, so there is no need to
    specify it.

--- terraform/ubuntu1404_ec2.tf
+++ terraform/ubuntu1404_ec2.tf
@@ -31,9 +31,8 @@ resource "aws_instance" "vuln_ubuntu1404" {
   associate_public_ip_address = true
 
   root_block_device {
-    volume_type           = "gp2"
-    volume_size           = 100
-    delete_on_termination = true
+    volume_size = 100
+    volume_type = "gp3"
   }
 
   vpc_security_group_ids = [
